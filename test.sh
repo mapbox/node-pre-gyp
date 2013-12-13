445d1436cb0afaa8a1d022c410f36b91be89585a
Dane Springmeyer <dane@dbsgeo.com>
2013-12-12 21:45:38 -0800

    improve test script

--- test.sh
+++ test.sh
@@ -3,22 +3,28 @@
 # put npm's copy of node-gyp on the PATH
 export PATH=`npm explore npm -g -- pwd`/bin/node-gyp-bin/:$PATH
 
-cd test/app1
+ROOTDIR=`pwd`/test
 
-# test normal install
-rm -rf build
-../../bin/node-pre-gyp.js rebuild
-node index.js
+function build_app {
+    cd $ROOTDIR/$1
+    # test normal install
+    rm -rf build
+    ../../bin/node-pre-gyp.js rebuild
+    node index.js
 
-# test source build
-rm -rf build
-../../bin/node-pre-gyp.js rebuild --build-from-source
-node index.js
+    # test source build
+    rm -rf build
+    ../../bin/node-pre-gyp.js rebuild --build-from-source
+    node index.js
 
-# test packaging
-rm -rf build
-../../bin/node-pre-gyp.js package
-rm -rf build
-mkdir -p build/Release
-tar xf stage/app1-v0.1.0-node-v11-darwin-x64.tar.gz -O > build/Release/app1.node
-node index.js
+    # test packaging
+    rm -rf build
+    rm -rf stage
+    ../../bin/node-pre-gyp.js package
+    rm -rf build
+    mkdir -p build/Release
+    tar xf stage/*.tar.gz -O > build/Release/$1.node
+    node index.js
+}
+
+build_app "app1"

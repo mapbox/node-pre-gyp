c0cb5479bb558802f66a25c42e1d560326c21fa7
Dane Springmeyer <dane@dbsgeo.com>
2014-01-14 18:28:57 -0800

    smarter require path logic

--- test/app3/index.js
+++ test/app3/index.js
@@ -1,8 +1,10 @@
 var path = require('path');
 var pkg = require('./package.json');
 var assert = require('assert');
-var binding = './' + path.join(pkg.binary.module_path,pkg.binary.module_name + '.node');
-var app = require(binding);
+var module_path = path.join(
+     path.relative(__dirname,pkg.binary.module_path),
+     pkg.binary.module_name + '.node');
+var app = require('./'+module_path);
 
 assert.ok(app);
 assert.equal(app.hello(),"hello");

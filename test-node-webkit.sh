5235103f0503931e98b3cfbcff445f114be89bf7
Dane Springmeyer <dane@mapbox.com>
2014-02-12 15:24:13 -0800

    Add timeout to testing of binary to ensure we close node-webkit gui

--- test-node-webkit.sh
+++ test-node-webkit.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 
+set -e -u
 
 if [[ `uname -s` == 'Darwin' ]]; then
     if [[ ! -f node-webkit-v0.8.4-osx-ia32.zip ]]; then
@@ -9,19 +11,17 @@ if [[ `uname -s` == 'Darwin' ]]; then
     fi
     export PATH=$(pwd)/node-webkit.app/Contents/MacOS/:${PATH}
     cd test/app1
+    rm -rf build/
+    rm -rf lib/binding/
     node-pre-gyp clean build --runtime=node-webkit --target=0.8.4
     node-pre-gyp package --runtime=node-webkit
     node-pre-gyp clean
-    killall node-webkit 2>/dev/null
-    node-pre-gyp testpackage --runtime=node-webkit --overwrite  2>/dev/null 1>/dev/null &
-    pid=$!
-    sleep 5
-    kill $pid
-    if [ $? != 0 ]; then
-      echo "Unable to start app"
-    else
-      echo "app started just fine"
-    fi
-    killall node-webkit 2>/dev/null
-#    ${NW} index.js
+    rm -rf lib/binding/
+    node-pre-gyp testpackage --runtime=node-webkit --overwrite 2>/dev/null 1>/dev/null &
+    # now test installing from remote
+    node-pre-gyp unpublish publish --runtime=node-webkit
+    node-pre-gyp clean
+    rm -rf build/
+    rm -rf lib/binding/
+    npm install --runtime=node-webkit
 fi

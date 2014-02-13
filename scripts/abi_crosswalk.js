c0de02f193c19e4c5934fb83b52af1bcf0c6d9de
Dane Springmeyer <dane@dbsgeo.com>
2014-02-12 19:08:06 -0800

    collect v8 version

--- scripts/abi_crosswalk.js
+++ scripts/abi_crosswalk.js
@@ -14,6 +14,7 @@ node scripts/abi_crosswalk.js > lib/util/abi_crosswalk.json
 var cross = {};
 
 var template = 'https://raw.github.com/joyent/node/v{VERSION}/src/';
+var v8template = 'https://raw.github.com/joyent/node/v{VERSION}/deps/v8/src/version.cc';
 
 var sortObjectByKey = function(obj){
     var keys = [];
@@ -69,15 +70,43 @@ function get(ver,callback) {
           value = value.slice(0,value.indexOf(' '));
         }
         var int_val = +value;
-        cross[ver] = int_val;
-        return callback(null,ver,int_val);
+        if (int_val != undefined) {
+          // TODO - if val is 1 then we need to get the v8 version from
+          // https://github.com/joyent/node/blob/master/deps/v8/src/version.cc
+          var v8path = v8template.replace('{VERSION}',ver);
+          var v8uri = url.parse(v8path);
+          https.get(v8uri, function(res) {
+              if (res.statusCode != 200) {
+                throw new Error("server returned " + res.statusCode + ' for: ' + path);
+              }
+              res.setEncoding('utf8');
+              var body = '';
+              res.on('data', function (chunk) {
+                body += chunk;
+              });
+              res.on('end',function(err) {
+                  var term = 'define MAJOR_VERSION'
+                  var idx = body.indexOf(term);
+                  var following = body.slice(idx);
+                  var end = following.indexOf('\n');
+                  var major = following.slice(term.length,end).trim();
+                  var term1 = 'define MINOR_VERSION'
+                  var idx1 = body.indexOf(term1);
+                  var following1 = body.slice(idx1);
+                  var end1 = following1.indexOf('\n');
+                  var minor = following1.slice(term1.length,end1).trim();
+                  var v8_version = major+'.'+minor;
+                  return callback(null,ver,int_val,v8_version);
+              });
+          });
+        }
       })
     });
 }
 
 process.on('exit', function(err) {
     var sorted = sortObjectByKey(cross);
-    console.log(JSON.stringify(sorted,null,1));
+    console.log(JSON.stringify(sorted,null,2));
 })
 
 var versions_doc = 'http://nodejs.org/dist/npm-versions.txt';
@@ -93,9 +122,15 @@ http.get(url.parse(versions_doc), function(res) {
     res.on('end',function(err) {
       var lines = body.split('\n').map(function(line) { return line.split(' ')[0].slice(1); }).filter(function(line) { return (line.length && line != 'node'); });
       lines.forEach(function(ver) {
-          get(ver,function(err,version,result) {
-            cross[version] = result;
+          get(ver,function(err,version,node_abi,v8_version) {
+            cross[version] = {node_abi:node_abi,v8:v8_version};
           });
       });
     });
 });
+
+/*
+get("0.8.1",function(err,version,node_abi,v8_version) {
+    cross[version] = {node_abi:node_abi,v8:v8_version};
+});
+*/

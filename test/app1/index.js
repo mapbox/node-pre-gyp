86c55668029206f3498efd2fc8f02c24d60c210c
Dane Springmeyer <dane@dbsgeo.com>
2014-03-07 18:13:05 -0800

    depend on bindings to make dynamic require less verbose

--- test/app1/index.js
+++ test/app1/index.js
@@ -1,6 +1,3 @@
-var binary = require('node-pre-gyp');
-var path = require('path')
-var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
-var binding = require(binding_path);
+var binding = require('node-pre-gyp').binding();
 
 require('assert').equal(binding.hello(),"hello");

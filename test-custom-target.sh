b9f1df94f5e88109a6ce517c2cfe3d9fb9ea6dc2
Dane Springmeyer <dane@dbsgeo.com>
2014-03-14 20:55:57 -0700

    test --target flag

--- test-custom-target.sh
+++ test-custom-target.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-set -e -u
+set -e
 
-if [[ `uname -s` == 'Darwin' ]]; then
-    export PATH=`pwd`/bin:$PATH
-    BASE=`pwd`
-    cd ${BASE}/test/app1
+# put local copy of node-pre-gyp on NODE_PATH/PATH
+export NODE_PATH=`pwd`/lib
+export PATH=`pwd`/bin:$PATH
+
+BASE=$(pwd)
+
+cd ${BASE}/test/app1
+source ~/.nvm/nvm.sh
+
+for i in {"0.8.26","0.10.26","0.11.7","0.11.10","0.11.11"}; do
     rm -rf build/
-    rm -rf lib/binding/
-    source ~/.nvm/nvm.sh
-    for i in $(ls ~/.node-gyp/); do
-        npm install --build-from-source --target=$i
-        node-pre-gyp package --target=$i
-        nvm use $i
-        node-pre-gyp testpackage --overwrite
-        /Users/dane/.nvm/v$i/bin/node ../../bin/node-pre-gyp testpackage --overwrite
-    done
-fi
+    node-pre-gyp clean
+    npm install --build-from-source --target=$i
+    node-pre-gyp package --target=$i
+    nvm install $i
+    nvm use $i
+    node-pre-gyp testpackage
+done

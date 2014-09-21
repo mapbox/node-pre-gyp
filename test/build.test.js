fc19f0b573ff89026f47275d3586d7ae7ec0c775
Dane Springmeyer <dane@mapbox.com>
2014-09-20 20:31:39 -0700

    port more tests to mocha

--- test/build.test.js
+++ test/build.test.js
@@ -11,10 +11,12 @@ if (process.platform === 'win32') {
 process.env.PATH = cmd_path + sep + process.env.PATH;
 process.env.NODE_PATH = path.join(__dirname,'../lib/');
 
-function run(command,app,cb) {
-    command += ' -C ' + path.join(__dirname,app.name);
+function run(command,app,opts,cb) {
+    if (!opts.cwd) {
+        command += ' -C ' + path.join(__dirname,app.name);
+    }
     command += ' ' + app.args;
-    cp.exec(command,cb);
+    cp.exec(command,opts, cb);
 }
 
 var apps = [
@@ -42,27 +44,110 @@ var apps = [
 
 describe('build', function() {
     apps.forEach(function(app) {
-        it('should build '+ app.name + ' ' + app.args, function(done) {
-            run('node-pre-gyp rebuild --fallback-to-build', app, function(err,stdout,stderr) {
+
+        it(app.name + ' builds ' + app.args, function(done) {
+            run('node-pre-gyp rebuild --fallback-to-build', app, {}, function(err,stdout,stderr) {
                 if (err) throw err;
                 assert.ok(stdout.search(app.name+'.node') > -1);
                 assert.equal(stderr,'');
-                run('node-pre-gyp reveal module_path --silent', app, function(err,stdout,stderr) {
+                done();
+            })
+        });
+
+        it(app.name + ' is found ' + app.args, function(done) {
+            run('node-pre-gyp reveal module_path --silent', app, {}, function(err,stdout,stderr) {
+                if (err) throw err;
+                assert.equal(stderr,'');
+                var module_path = stdout.trim();
+                assert.ok(module_path.search(app.name) > -1);
+                assert.ok(existsSync(module_path));
+                var module_binary = path.join(module_path,app.name+'.node');
+                assert.ok(existsSync(module_binary));
+                done();
+            });
+        });
+
+        it(app.name + ' passes tests ' + app.args, function(done) {
+            run('npm test', app, {cwd: path.join(__dirname,app.name)}, function(err,stdout,stderr) {
+                if (err) throw err;
+                assert.equal(stderr,'');
+                assert.notEqual(stdout,'');
+                done();
+            });
+        });
+
+        it(app.name + ' packages ' + app.args, function(done) {
+            run('node-pre-gyp package', app, {}, function(err,stdout,stderr) {
+                if (err) throw err;
+                assert.equal(stderr,'');
+                done();
+            });
+        });
+
+        it(app.name + ' package is valid ' + app.args, function(done) {
+            run('node-pre-gyp testpackage', app, {}, function(err,stdout,stderr) {
+                if (err) throw err;
+                assert.equal(stderr,'');
+                done();
+            });
+        });
+
+        if (process.env.AWS_ACCESS_KEY_ID) {
+
+            it(app.name + ' publishes ' + app.args, function(done) {
+                run('node-pre-gyp unpublish publish', app, {}, function(err,stdout,stderr) {
                     if (err) throw err;
                     assert.equal(stderr,'');
-                    var module_path = stdout.trim();
-                    assert.ok(module_path.search(app.name) > -1);
-                    assert.ok(existsSync(module_path));
-                    var module_binary = path.join(module_path,app.name+'.node');
-                    assert.ok(existsSync(module_binary));
-                    run('npm test', app, function(err,stdout,stderr) {
+                    assert.notEqual(stdout,'');
+                    done();
+                });
+            });
+
+            it(app.name + ' info shows it ' + app.args, function(done) {
+                run('node-pre-gyp reveal package_name --silent', app, {}, function(err,stdout,stderr) {
+                    if (err) throw err;
+                    assert.equal(stderr,'');
+                    assert.notEqual(stdout,'');
+                    var package_name = stdout.trim();
+                    run('node-pre-gyp info', app, {}, function(err,stdout,stderr) {
                         if (err) throw err;
                         assert.equal(stderr,'');
-                        assert.notEqual(stdout,'');
+                        assert.ok(stdout.search(package_name) > -1);
                         done();
                     });
                 });
-            })
-        });
+            });
+
+            it(app.name + ' can be uninstalled ' + app.args, function(done) {
+                run('node-pre-gyp clean', app, {}, function(err,stdout,stderr) {
+                    if (err) throw err;
+                    assert.equal(stderr,'');
+                    assert.notEqual(stdout,'');
+                    done();
+                });
+            });
+
+            it(app.name + ' can be installed via remote ' + app.args, function(done) {
+                run('npm install --fallback-to-build=false', app, {cwd: path.join(__dirname,app.name)}, function(err,stdout,stderr) {
+                    if (err) throw err;
+                    assert.equal(stderr,'');
+                    assert.notEqual(stdout,'');
+                    done();
+                });
+            });
+
+            it(app.name + ' via remote passes tests ' + app.args, function(done) {
+                run('npm test', app, {cwd: path.join(__dirname,app.name)}, function(err,stdout,stderr) {
+                    if (err) throw err;
+                    assert.equal(stderr,'');
+                    assert.notEqual(stdout,'');
+                    done();
+                });
+            });
+
+        } else {
+            it.skip(app.name + ' publishes ' + app.args, function() {});
+        }
+
     });
 });

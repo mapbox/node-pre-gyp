46ac83125e7eecac53dea4f26bf969d81ce8510f
Dane Springmeyer <dane@mapbox.com>
2015-07-13 17:48:08 -0700

    add latest versions

--- scripts/abi_crosswalk.js
+++ scripts/abi_crosswalk.js
@@ -121,13 +121,13 @@ if (update_node) {
   for (var i=0;i<=28;++i) {
     lines.push('0.8.'+i);
   }
-  for (var i=0;i<=39;++i) {
+  for (var i=0;i<=40;++i) {
     lines.push('0.10.'+i);
   }
   for (var i=0;i<=16;++i) {
     lines.push('0.11.'+i);
   }
-  for (var i=0;i<=6;++i) {
+  for (var i=0;i<=7;++i) {
     lines.push('0.12.'+i);
   }
   lines.forEach(function(ver) {

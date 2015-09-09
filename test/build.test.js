41d87fc286d8816c27a6e5004e52daf12e18a6db
Dane Springmeyer <dane@mapbox.com>
2015-09-09 14:41:17 -0700

    add clang command earlier on

--- test/build.test.js
+++ test/build.test.js
@@ -24,14 +24,14 @@ function run(prog,command,args,app,opts,cb) {
     if (!opts.cwd) {
         final_cmd += ' -C ' + path.join(__dirname,app.name);
     }
+    // clang=1 is harmless to add, but avoids breakage on `-fno-tree-sink`
+    // flag that nodejs gyp scripts add and clang does not know about
+    final_cmd += ' --clang=1';
     if (process.platform === 'win32') {
         final_cmd += ' --msvs_version=2013 ';
     }
     final_cmd += ' ' + app.args;
     final_cmd += ' ' + args;
-    // clang=1 is harmless to add, but avoids breakage on `-fno-tree-sink`
-    // flag that nodejs gyp scripts add and clang does not know about
-    final_cmd += ' --clang=1';
     cp.exec(final_cmd,opts,function(err,stdout,stderr) {
         if (err) {
             var error = new Error("Command failed '" + command + "'");

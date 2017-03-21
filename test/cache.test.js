ec2cb6bf6543d3518f7dc32c2dcb8a0a81a1381c
Bryce Gibson <bryce@gibson-consulting.com.au>
2017-03-22 08:11:41 +1100

    Add local builds into the cache.

--- test/cache.test.js
+++ test/cache.test.js
@@ -5,22 +5,27 @@ var path = require('path');
 var fs = require('fs');
 var cp = require('child_process');
 
-var default_cmd = path.join(__dirname,'..','bin','node-pre-gyp') + ' install --update-binary --fallback-to-build=false';
+var binPath = path.join(__dirname,'..','bin','node-pre-gyp');
+var install_cmd = binPath + ' install --update-binary --fallback-to-build=false';
 
 var cache_dir = path.join(__dirname, 'cache');
 var test_dir = path.join(__dirname, 'app1');
 
-function install(cmd,opts,callback) {
-    if(typeof cmd === 'function') {
-        callback = cmd;
-        cmd = default_cmd;
-    }
+function run(cmd,opts,callback) {
     if(typeof opts === 'function') {
         callback = opts;
         opts = null;
     }
     var test_env = Object.assign({}, process.env, { NODE_PRE_GYP_CACHE: cache_dir }, opts);
     cp.exec(cmd, { cwd: test_dir, env: test_env }, callback);
+}
+
+function install(cmd,opts,callback) {
+    if(typeof cmd === 'function') {
+        callback = cmd;
+        cmd = install_cmd;
+    }
+    run(cmd, opts, callback);
 }
 
 function on_error(err,stdout,stderr) {
@@ -35,13 +40,17 @@ function on_error(err,stdout,stderr) {
         // cleanup
         fs.mkdir(cache_dir, function(err) {
             if(err && err.code !== 'EEXIST') throw err;
-            fs.readdir(cache_dir, function(err, files) {
-                if(err) throw err;
-                files.forEach(function(file) {
-                    fs.unlinkSync(path.join(cache_dir,file));
-                });
-                done();
+            done();
+        });
+    });
+
+    beforeEach(function(done) {
+        fs.readdir(cache_dir, function(err, files) {
+            if(err) throw err;
+            files.forEach(function(file) {
+                fs.unlinkSync(path.join(cache_dir,file));
             });
+            done();
         });
     });
 
@@ -62,7 +71,7 @@ function on_error(err,stdout,stderr) {
             fs.readdir(cache_dir, function(err, files) {
                 if(err) throw err;
                 var mtime = fs.statSync(path.join(cache_dir,files[0])).mtime;
-                install(default_cmd + ' --loglevel=verbose', function(err,stdout,stderr) {
+                install(install_cmd + ' --loglevel=verbose', function(err,stdout,stderr) {
                     assert.ok(stderr.indexOf('from cache') > -1);
                     assert.equal(stderr.indexOf('http'), -1);
                     assert.equal(fs.statSync(path.join(cache_dir,files[0])).mtime.getTime(), mtime.getTime(), 'cache value was not updated');
@@ -97,23 +106,75 @@ function on_error(err,stdout,stderr) {
             fs.readdir(cache_dir, function(err, files) { // populate cache
                 if(err) throw err;
                 var mtime = fs.statSync(path.join(cache_dir,files[0])).mtime;
-                install(default_cmd + ' --ignore-cache', function(err,stdout,stderr) {
+                install(install_cmd + ' --ignore-cache', function(err,stdout,stderr) {
                     if(err) return on_error(err,stdout,stderr);
                     assert.equal(stderr.indexOf('from cache'), -1);
-                    fs.readdir(cache_dir, function(err, files) {
-                        assert.ok(fs.statSync(path.join(cache_dir,files[0])).mtime.getTime() > mtime.getTime(), 'cache value was not used, but was instead updated');
-                        done();
-                    });
+                    assert.ok(fs.statSync(path.join(cache_dir,files[0])).mtime.getTime() > mtime.getTime(), 'cache value was not used, but was instead updated');
+                    done();
+                });
+            });
+        });
+    });
+
+    it('ignores the cache and doesn\'t update it if told to', function(done) {
+        install(function(err,stdout,stderr) {
+            if(err) return on_error(err,stdout,stderr);
+            fs.readdir(cache_dir, function(err, files) { // populate cache
+                if(err) throw err;
+                var mtime = fs.statSync(path.join(cache_dir,files[0])).mtime;
+                install(install_cmd + ' --ignore-cache --skip-cache', function(err,stdout,stderr) {
+                    if(err) return on_error(err,stdout,stderr);
+                    assert.equal(stderr.indexOf('from cache'), -1);
+                    assert.equal(fs.statSync(path.join(cache_dir,files[0])).mtime.getTime(), mtime.getTime(), 'cache value was not updated');
+                    done();
                 });
             });
         });
     });
 
     it('proceeds even when the cache dir can\'t be accessed/created', function(done) {
-        install(default_cmd + ' --loglevel=verbose', { NODE_PRE_GYP_CACHE: path.join(cache_dir, 'x', 'x') }, function(err,stdout,stderr) {
+        install(install_cmd + ' --loglevel=verbose', { NODE_PRE_GYP_CACHE: path.join(cache_dir, 'x', 'x') }, function(err,stdout,stderr) {
             if(err) return on_error(err,stdout,stderr);
             assert.ok(stderr.indexOf('http') > -1, 'fetched binding');
             done();
+        });
+    });
+
+    it('populates the cache with a local build', function(done) {
+        run(binPath + ' rebuild --fallback-to-build --loglevel=error', function(err,stdout,stderr) {
+            if(err) return on_error(err,stdout,stderr);
+            fs.readdir(cache_dir, function(err, files) {
+                if(err) throw err;
+                assert.equal(files.length, 1);
+                done();
+            });
+        });
+    });
+
+    it('doesn\'t write to the cache when told not to', function(done) {
+        run(binPath + ' rebuild --fallback-to-build --loglevel=error --skip-cache', function(err,stdout,stderr) {
+            if(err) return on_error(err,stdout,stderr);
+            fs.readdir(cache_dir, function(err, files) {
+                if(err) throw err;
+                assert.equal(files.length, 0);
+                done();
+            });
+        });
+    });
+
+    it('reuses a cached local build', function(done) {
+        run(binPath + ' rebuild --fallback-to-build --loglevel=error', function(err,stdout,stderr) {
+            if(err) return on_error(err,stdout,stderr);
+            fs.readdir(cache_dir, function(err, files) {
+                if(err) throw err;
+                var mtime = fs.statSync(path.join(cache_dir,files[0])).mtime;
+                install(install_cmd + ' --loglevel=verbose', function(err,stdout,stderr) {
+                    assert.ok(stderr.indexOf('from cache') > -1);
+                    assert.equal(stderr.indexOf('http'), -1);
+                    assert.equal(fs.statSync(path.join(cache_dir,files[0])).mtime.getTime(), mtime.getTime(), 'cache value was not updated');
+                    done();
+                });
+            });
         });
     });
 });

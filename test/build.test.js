c43bbcd48bbb4f581496b944341f47cee4ae8d3f
Dane Springmeyer <dane@mapbox.com>
2017-06-11 09:53:00 -0400

    repair failing tests after refactoring

--- test/build.test.js
+++ test/build.test.js
@@ -1,52 +1,14 @@
 "use strict";
 
 var test = require('tape');
-var cp = require('child_process');
-var path = require('path');
+var run = require('./run.util.js');
 var existsSync = require('fs').existsSync || require('path').existsSync;
-var abi_crosswalk = require('../lib/util/abi_crosswalk.json');
-var os = require('os');
 var fs = require('fs');
 var rm = require('rimraf');
+var path = require('path');
+var getPrevious = require('./target_version.util.js');
 
-var cmd_path = path.join(__dirname,'../bin/');
-var sep = ':';
-var propertyPrefix = '';
-if (process.platform === 'win32') {
-    sep = ';';
-    propertyPrefix = '/p:';
-}
-process.env.PATH = cmd_path + sep + process.env.PATH;
-process.env.NODE_PATH = path.join(__dirname,'../lib/');
-
-function run(prog,command,args,app,opts,cb) {
-    var final_cmd = prog + ' ' + command;
-    if (!opts.cwd) {
-        final_cmd += ' -C ' + path.join(__dirname,app.name);
-    }
-    // clang=1 is harmless to add, but avoids breakage on `-fno-tree-sink`
-    // flag that nodejs gyp scripts add and clang does not know about
-    final_cmd += ' --clang=1';
-    if (process.platform === 'win32') {
-        final_cmd += ' --msvs_version=2015 ';
-    }
-    final_cmd += ' ' + app.args;
-    final_cmd += ' ' + args;
-    // on unix display compile args
-    if (process.platform !== 'win32') {
-        final_cmd = 'V=1 ' + final_cmd;
-    }
-
-    cp.exec(final_cmd,opts,function(err,stdout,stderr) {
-        if (err) {
-            var error = new Error("Command failed '" + final_cmd + "'");
-            error.stack = stderr;
-            return cb(error,stdout,stderr);
-        }
-        return cb(err,stdout,stderr);
-    });
-}
-
+// The list of different sample apps that we use to test
 var apps = [
     {
         'name': 'app1',
@@ -70,35 +32,6 @@ var apps = [
     }
 ];
 
-function getPreviousVersion(current_version) {
-    var current_parts = current_version.split('.').map(function(i) { return +i; });
-    var major = current_parts[0];
-    var minor = current_parts[1];
-    var patch = current_parts[2];
-    while (patch > 0) {
-        --patch;
-        var new_target = '' + major + '.' + minor + '.' + patch;
-        if (new_target == current_version) {
-            break;
-        }
-        if (abi_crosswalk[new_target]) {
-            return new_target;
-        }
-    }
-    // failed to find suitable future version that we expect is ABI compatible
-    return undefined;
-}
-
-var current_version = process.version.replace('v','');
-var previous_version = getPreviousVersion(current_version);
-var target_abi;
-var testing_crosswalk;
-if (previous_version !== undefined && previous_version !== current_version) {
-    target_abi = {};
-    target_abi[previous_version] = abi_crosswalk[previous_version];
-    testing_crosswalk = path.join(os.tmpdir(),'fake_abi_crosswalk.json');
-    fs.writeFileSync(testing_crosswalk,JSON.stringify(target_abi));
-}
 
 // https://stackoverflow.com/questions/38599457/how-to-write-a-custom-assertion-for-testing-node-or-javascript-with-tape-or-che
 test.Test.prototype.stringContains = function(actual, contents, message) {
@@ -109,6 +42,65 @@ test.Test.prototype.stringContains = function(actual, contents, message) {
     expected: contents
   });
 };
+
+// Because the below tests only ensure that flags can be correctly passed to node-gyp is it not
+// likely they will behave differently for different apps. So we save time by avoiding running these for each app.
+var app = apps[0];
+
+// make sure node-gyp options are passed by passing invalid values
+// and ensuring the expected errors are returned from node-gyp
+test(app.name + ' passes --nodedir down to node-gyp via node-pre-gyp ' + app.args, function(t) {
+    run('node-pre-gyp', 'configure', '--nodedir=invalid-value', app, {}, function(err,stdout,stderr) {
+        t.ok(err,'Expected command to fail');
+        t.stringContains(stderr,"common.gypi not found");
+        t.end();
+    });
+});
+
+test(app.name + ' passes --nodedir down to node-gyp via npm' + app.args, function(t) {
+    run('npm', 'install', '--build-from-source --nodedir=invalid-value', app, {}, function(err,stdout,stderr) {
+        t.ok(err, 'Expected command to fail');
+        t.stringContains(stderr,"common.gypi not found");
+        t.end();
+    });
+});
+
+// TODO - for some reason these do not error on windows
+if (process.platform !== 'win32') {
+    test(app.name + ' passes --python down to node-gyp via node-pre-gyp ' + app.args, function(t) {
+        run('node-pre-gyp', 'configure', '--python=invalid-value', app, {}, function(err,stdout,stderr) {
+            t.ok(err, 'Expected command to fail');
+            t.stringContains(stderr,"Can't find Python executable");
+            t.end();
+        });
+    });
+
+    test(app.name + ' passes --python down to node-gyp via npm ' + app.args, function(t) {
+        run('node-pre-gyp', 'configure', '--build-from-source --python=invalid-value', app, {}, function(err,stdout,stderr) {
+            t.ok(err, 'Expected command to fail');
+            t.stringContains(stderr,"Can't find Python executable");
+            t.end();
+        });
+    });
+}
+// note: --ensure=false tells node-gyp to attempt to re-download the node headers
+// even if they already exist on disk at ~/.node-gyp/{version}
+test(app.name + ' passes --dist-url down to node-gyp via node-pre-gyp ' + app.args, function(t) {
+    run('node-pre-gyp', 'configure', '--ensure=false --dist-url=invalid-value', app, {}, function(err,stdout,stderr) {
+        t.ok(err, 'Expected command to fail');
+        t.end();
+    });
+});
+
+test(app.name + ' passes --dist-url down to node-gyp via npm ' + app.args, function(t) {
+    run('npm', 'install', '--build-from-source --ensure=false --dist-url=invalid-value', app, {}, function(err,stdout,stderr) {
+        t.ok(err, 'Expected command to fail');
+        t.end();
+    });
+});
+
+
+// Tests run for all apps
 
 apps.forEach(function(app) {
 
@@ -139,11 +131,24 @@ apps.forEach(function(app) {
             });
         });
 
-        test(app.name + ' installs', function(t) {
-            run('node-pre-gyp', 'install', '--update-binary --fallback-to-build', app, {}, function(err,stdout,stderr) {
+        test(app.name + ' builds with unparsed options ' + app.args, function(t) {
+            // clean and build as separate steps here because configure only works with -Dfoo=bar
+            // and build only works with FOO=bar
+            run('node-pre-gyp', 'clean', '', app, {}, function(err) {
                 t.ifError(err);
-                t.stringContains(stdout,'Success: ');
-                t.end();
+                var propertyPrefix = (process.platform === 'win32') ? '/p:' : '';
+                run('node-pre-gyp', 'build', '--loglevel=info -- ' + propertyPrefix + 'FOO=bar', app, {}, function(err,stdout,stderr) {
+                    t.ifError(err);
+                    t.ok(stderr.search(/(gyp info spawn args).*(FOO=bar)/) > -1);
+                    if (process.platform !== 'win32') {
+                        if (app.args.indexOf('--debug') > -1) {
+                            t.stringContains(stdout,'Debug/'+app.name+'.node');
+                        } else {
+                            t.stringContains(stdout,'Release/'+app.name+'.node');
+                        }
+                    }
+                    t.end();
+                });
             });
         });
 
@@ -165,8 +170,8 @@ apps.forEach(function(app) {
             run('node-pre-gyp', 'reveal', 'module_path --silent', app, {}, function(err,stdout,stderr) {
                 t.ifError(err);
                 var module_path = stdout.trim();
-                t.ok(module_path.search(app.name) > -1);
-                t.ok(existsSync(module_path),'is found '+ module_path);
+                t.stringContains(module_path,app.name);
+                t.ok(existsSync(module_path),'is valid path to existing binary: '+ module_path);
                 var module_binary = path.join(module_path,app.name+'.node');
                 t.ok(existsSync(module_binary));
                 t.end();
@@ -174,11 +179,10 @@ apps.forEach(function(app) {
         });
 
         test(app.name + ' passes tests ' + app.args, function(t) {
-            run('npm','test','', app, {env : process.env, cwd: path.join(__dirname,app.name)}, function(err,stdout,stderr) {
+            run('npm','test','', app, {cwd: path.join(__dirname,app.name)}, function(err,stdout,stderr) {
                 t.ifError(err);
                 // we expect app2 to console.log on success
                 if (app.name == 'app2') {
-                    console.log(stdout);
                     if (app.args.indexOf('--debug') > -1) {
                         t.stringContains(stdout,'Loaded Debug build');
                     } else {
@@ -237,7 +241,7 @@ apps.forEach(function(app) {
             });
 
             test(app.name + ' can be installed via remote ' + app.args, function(t) {
-                run('npm', 'install', '--fallback-to-build=false', app, {env : process.env, cwd: path.join(__dirname,app.name)}, function(err,stdout,stderr) {
+                run('npm', 'install', '--fallback-to-build=false', app, {cwd: path.join(__dirname,app.name)}, function(err,stdout,stderr) {
                     t.ifError(err);
                     t.notEqual(stdout,'');
                     t.end();
@@ -245,7 +249,7 @@ apps.forEach(function(app) {
             });
 
             test(app.name + ' can be reinstalled via remote ' + app.args, function(t) {
-                run('npm', 'install', '--update-binary --fallback-to-build=false', app, {env : process.env, cwd: path.join(__dirname,app.name)}, function(err,stdout,stderr) {
+                run('npm', 'install', '--update-binary --fallback-to-build=false', app, {cwd: path.join(__dirname,app.name)}, function(err,stdout,stderr) {
                     t.ifError(err);
                     t.notEqual(stdout,'');
                     t.end();
@@ -253,7 +257,7 @@ apps.forEach(function(app) {
             });
 
             test(app.name + ' via remote passes tests ' + app.args, function(t) {
-                run('npm', 'install', '', app, {env : process.env, cwd: path.join(__dirname,app.name)}, function(err,stdout,stderr) {
+                run('npm', 'install', '', app, {cwd: path.join(__dirname,app.name)}, function(err,stdout,stderr) {
                     t.ifError(err);
                     t.notEqual(stdout,'');
                     t.end();
@@ -262,94 +266,6 @@ apps.forEach(function(app) {
 
         } else {
             test.skip(app.name + ' publishes ' + app.args, function() {});
-        }
-
-        test(app.name + ' builds with unparsed options ' + app.args, function(t) {
-            run('node-pre-gyp', 'clean', '', app, {}, function(err,stdout,stderr) {
-                t.ifError(err);
-                run('node-pre-gyp', 'build', '--loglevel=info -- ' + propertyPrefix + 'FOO=bar', app, {}, function(err,stdout,stderr) {
-                    t.ifError(err);
-                    t.ok(stderr.search(/(gyp info spawn args).*(FOO=bar)/) > -1);
-                    t.end();
-                });
-            });
-        });
-
-        // make sure node-gyp options are passed by passing invalid values
-        // and ensuring the expected errors are returned from node-gyp
-        //Python executable "foo"
-        test(app.name + ' passes --nodedir down to node-gyp via node-pre-gyp ' + app.args, function(t) {
-            run('node-pre-gyp', 'configure', '--nodedir=invalid-value', app, {}, function(err,stdout,stderr) {
-                t.ok(err);
-                t.stringContains(stderr,"common.gypi not found");
-                t.end();
-            });
-        });
-
-        test(app.name + ' passes --nodedir down to node-gyp via npm' + app.args, function(t) {
-            run('npm', 'install', '--build-from-source --nodedir=invalid-value', app, {}, function(err,stdout,stderr) {
-                t.ok(err);
-                t.stringContains(stderr,"common.gypi not found");
-                t.end();
-            });
-        });
-
-        // TODO - for some reason these do not error on windows
-        if (process.platform !== 'win32') {
-            test(app.name + ' passes --python down to node-gyp via node-pre-gyp ' + app.args, function(t) {
-                run('node-pre-gyp', 'configure', '--python=invalid-value', app, {}, function(err,stdout,stderr) {
-                    t.ok(err);
-                    t.stringContains(stderr,"Can't find Python executable");
-                    t.end();
-                });
-            });
-
-            test(app.name + ' passes --python down to node-gyp via npm ' + app.args, function(t) {
-                run('node-pre-gyp', 'configure', '--build-from-source --python=invalid-value', app, {}, function(err,stdout,stderr) {
-                    t.ok(err);
-                    t.stringContains(stderr,"Can't find Python executable");
-                    t.end();
-                });
-            });
-        }
-        // note: --ensure=false tells node-gyp to attempt to re-download the node headers
-        // even if they already exist on disk at ~/.node-gyp/{version}
-        test(app.name + ' passes --dist-url down to node-gyp via node-pre-gyp ' + app.args, function(t) {
-            run('node-pre-gyp', 'configure', '--ensure=false --dist-url=invalid-value', app, {}, function(err,stdout,stderr) {
-                t.ok(err);
-                t.end();
-            });
-        });
-
-        test(app.name + ' passes --dist-url down to node-gyp via npm ' + app.args, function(t) {
-            run('npm', 'install', '--build-from-source --ensure=false --dist-url=invalid-value', app, {}, function(err,stdout,stderr) {
-                t.ok(err);
-                t.end();
-            });
-        });
-
-        if (target_abi) {
-            var new_env = JSON.parse(JSON.stringify(process.env));
-            new_env.NODE_PRE_GYP_ABI_CROSSWALK = testing_crosswalk;
-            var opts = { env : new_env };
-            test(app.name + ' builds with custom --target='+previous_version+' that is greater than known version in ABI crosswalk ' + app.args, function(t) {
-                run('node-pre-gyp', 'rebuild', '--loglevel=error --fallback-to-build --target='+previous_version, app, opts, function(err,stdout,stderr) {
-                    t.ifError(err);
-                    t.end();
-                });
-            });
-
-            test(app.name + ' cleans up after installing custom --target='+previous_version+' that is greater than known in ABI crosswalk ' + app.args, function(t) {
-                run('node-pre-gyp', 'clean', '--target='+previous_version, app, opts, function(err,stdout,stderr) {
-                    t.ifError(err);
-                    t.notEqual(stdout,'');
-                    t.end();
-                });
-            });
-
-        } else {
-            test.skip(app.name + ' builds with custom --target that is greater than known in ABI crosswalk ' + app.args, function() {});
-            test.skip(app.name + ' builds with custom --target='+previous_version+' that is greater than known in ABI crosswalk ' + app.args, function() {});
         }
 
         // note: the above test will result in a non-runnable binary, so the below test must succeed otherwise all following tests will fail

1e116580506f020d9c7eead4c168a03c506d512b
Dane Springmeyer <dane@mapbox.com>
2021-01-01 20:38:06 -0800

    allow setting object acl

--- cloudformation/node-pre-gyp.template.js
+++ cloudformation/node-pre-gyp.template.js
@@ -5,5 +5,42 @@ const bucketName = require('./bucketName');
 
 module.exports = s3Template.build({
   BucketName: bucketName,
-  PublicAccessBlock: false
+  PublicAccessBlock: false,
+  BucketPolicy: {
+    'Type': 'AWS::S3::BucketPolicy',
+    'DependsOn': 'Bucket',
+    'Properties': {
+      'Bucket': bucketName,
+      'PolicyDocument': {
+        'Statement': [
+          {
+            'Sid': 'Allow setting Object ACLs',
+            'Effect': 'Allow',
+            'Principal': {
+              'AWS': '*'
+            },
+            'Action': [
+              's3:PutObjectAcl'
+            ],
+            'Resource': [
+              {
+                'Fn::Join': [
+                  '',
+                  [
+                    'arn:',
+                    {
+                      'Ref': 'AWS::Partition'
+                    },
+                    ':s3:::',
+                    bucketName,
+                    '/*'
+                  ]
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
 });

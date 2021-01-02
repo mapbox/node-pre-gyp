c71be62497e4bc0874b95b92fa12ba7d17571f2a
Dane Springmeyer <dane@mapbox.com>
2021-01-01 18:55:32 -0800

    more attempts at node v12 test fixes

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -8,12 +8,12 @@ test('should follow redirects', (t) => {
   // Dummy tar.gz data, contains a blank directory.
   const targz = 'H4sICPr8u1oCA3kudGFyANPTZ6A5MDAwMDc1VQDTZhAaCGA0hGNobGRqZm5uZmxupGBgaGhiZsKgYMpAB1BaXJJYBHRKYk5pcioedeUZqak5+D2J5CkFhlEwCkbBKBjkAAAyG1ofAAYAAA==';
 
-  const pattern = /\/app1-v0.1.0-node-v\d+-\S+.tar.gz/;
+  const pattern = /app1-v0.1.0-node-v\d+-\S+.tar.gz/;
   // Mock an HTTP redirect
   const n = nock('https://node-pre-gyp-tests.s3-us-west-1.amazonaws.com')
     .persist()
     .get(pattern)
-    .reply(302, '', {
+    .reply(302, undefined, {
       'Location': 'https://node-pre-gyp-tests.s3-us-west-1.amazonaws.com/otherapp.tar.gz'
     })
     .get('/otherapp.tar.gz')

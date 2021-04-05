d2fa223a28861b691e6ff37558729dc064141663
Pierre Veber <pveber@gmail.com>
2021-04-05 14:19:55 +0200

    [tests] Allow alternative s3 urls and s3-compatible backends (e.g. minio).

--- test/s3_setup.test.js
+++ test/s3_setup.test.js
@@ -4,9 +4,11 @@ const s3_setup = require('../lib/util/s3_setup.js');
 const test = require('tape');
 
 test('should propertly detect s3 bucket and prefix', (t) => {
-  const url = 'https://bucket-with-dashes.s3-us-west-1.amazonaws.com';
+  const opts = {
+    hosted_path: 'https://bucket-with-dashes.s3-us-west-1.amazonaws.com'
+  };
   const result = {};
-  s3_setup.detect(url, result);
+  s3_setup.detect(opts, result);
   t.equal(result.prefix, '');
   t.equal(result.bucket, 'bucket-with-dashes');
   t.equal(result.region, 'us-west-1');
@@ -14,9 +16,11 @@ test('should propertly detect s3 bucket and prefix', (t) => {
 });
 
 test('should propertly detect s3 bucket and prefix with dots', (t) => {
-  const url = 'https://bucket.with.dots.s3.amazonaws.com/prefix';
+  const opts = {
+    hosted_path: 'https://bucket.with.dots.s3.amazonaws.com/prefix'
+  };
   const result = {};
-  s3_setup.detect(url, result);
+  s3_setup.detect(opts, result);
   t.equal(result.prefix, 'prefix');
   t.equal(result.bucket, 'bucket.with.dots');
   t.equal(result.region, 'us-east-1');
@@ -24,11 +28,29 @@ test('should propertly detect s3 bucket and prefix with dots', (t) => {
 });
 
 test('should propertly detect modern s3 bucket and prefix', (t) => {
-  const url = 'https://bucket-name.s3.us-east-1.amazonaws.com';
+  const opts = {
+    hosted_path: 'https://bucket-name.s3.us-east-1.amazonaws.com'
+  };
   const result = {};
-  s3_setup.detect(url, result);
+  s3_setup.detect(opts, result);
   t.equal(result.prefix, '');
   t.equal(result.bucket, 'bucket-name');
   t.equal(result.region, 'us-east-1');
+  t.end();
+});
+
+test('should propertly detect compatible s3 bucket and prefix', (t) => {
+  const opts = {
+    host: 'https://storage.com',
+    hosted_path: 'https://storage.com',
+    bucket: 'bucket-name',
+    region: 'us-east-1'
+  };
+  const result = {};
+  s3_setup.detect(opts, result);
+  t.equal(result.prefix, '');
+  t.equal(result.bucket, 'bucket-name');
+  t.equal(result.region, 'us-east-1');
+  t.equal(result.endpoint, 'https://storage.com');
   t.end();
 });

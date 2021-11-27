a92c6e4c043c7f7b0fad5319dbe22ec427381854
dmarcs <marcusdavid127@gmail.com>
2021-11-27 15:40:17 -0500

    make this work with webpack

--- test/app1/index.js
+++ test/app1/index.js
@@ -4,3 +4,11 @@ var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json'
 var binding = require(binding_path);
 
 require('assert').equal(binding.hello(),"hello");
+
+var binding_path_using_package_json = binary.findUsingPackageJson(require('./package.json'));
+var binding_using_package_json = require('./' + path.join(
+  binding_path_using_package_json.module_relative_path, 
+  binding_path_using_package_json.module_name
+) + '.node');
+
+require('assert').equal(binding_using_package_json.hello(),"hello");

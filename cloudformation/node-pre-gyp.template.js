4de404ef6539081ccb7d251512c4d2d0646c9aa1
mapsam <matthews.sam@gmail.com>
2022-06-13 11:13:24 -0700

    allow acl on objects, allow access to objects

--- cloudformation/node-pre-gyp.template.js
+++ cloudformation/node-pre-gyp.template.js
@@ -1,31 +1,49 @@
-const s3Template = require('@mapbox/s3-bucket-template');
-const cf = require('@mapbox/cloudfriend');
+'use strict';
 
-const template = s3Template.build({
-  BucketName: 'mapbox-node-pre-gyp-public-testing-bucket',
-  PublicAccessBlockOptions: {
-    BlockPublicAcls: true,
-    IgnorePublicAcls: true,
-    BlockPublicPolicy: false,
-    RestrictPublicBuckets: false,
-  },
+const s3Template = require('@mapbox/s3-bucket-template');
+
+const bucket = s3Template.build({
+  BucketName: 'mapbox-node-binary',
+  // We allow public access to objects IF they have been set to "public-read"
+  // node-pre-gyp sets public-read when publishing binaries:
+  // https://github.com/mapbox/node-pre-gyp/blob/eb1ec94913cbb72acbf2a4dc69896581d131fef8/lib/publish.js#L46-L51
+  PublicAccessBlock: false,
+  // The custom policy requires users to s3:PutObjectAcl when publishing
+  // binaries. Otherwise these objects cannot be downloaded by the public internet
+  // when installing.
   BucketPolicy: {
     Type: 'AWS::S3::BucketPolicy',
+    DependsOn: 'Bucket',
     Properties: {
-      Bucket: cf.ref('Bucket'),
+      Bucket: 'mapbox-node-binary',
       PolicyDocument: {
         Statement: [
           {
-            Effect: 'Allow',
-            Action: 's3:GetObject',
-            Principal: '*',
-            Resource: 'arn:aws:s3:::mapbox-node-pre-gyp-public-testing-bucket/*',
+            Sid: 'Deny changing the bucket ACL',
+            Effect: 'Deny',
+            Principal: {
+              AWS: '*'
+            },
+            Action: 's3:PutBucketAcl',
+            Resource: 'arn:aws:s3:::mapbox-node-binary'
           },
-        ],
-      },
-    },
-  },
+          // prevent deletions of any kind (human or application) on this bucket
+          // even though node-pre-gyp allows for binaries to be deleted, Mapbox
+          // intends to prevent them from being removed.
+          {
+            Sid: 'DenyDeletions',
+            Effect: 'Deny',
+            Principal: '*',
+            Action: 's3:Delete*',
+            Resource: [
+              'arn:aws:s3:::mapbox-node-binary',
+              'arn:aws:s3:::mapbox-node-binary/*'
+            ]
+          }
+        ]
+      }
+    }
+  }
 });
 
-delete template.Resources.Bucket.Properties.AccessControl;
-module.exports = template;
+module.exports = bucket;

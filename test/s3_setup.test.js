60d6a894bb769f209980a5f0e395d4178c5c13be
Ron Ilan <ronilan@users.noreply.github.com>
2024-06-29 06:32:51 +0000

    Fix 607 Use s3ForcePathStyle Appropriately (#650)
    * Refactor S3 Setup detect function
    - Modified function signature. Instead of augmenting an object (side effect) the function now gets an options object and returns a configuration object.
    - Cleaned up of code auto used to extract bucket and region from s3 url.
    - Added comments.
    - Increased test coverage.
    - All tests pass.
    
    * Fixing issue 607
    - Modified versioning to add bucket name to hosted_path when s3ForcePathStyle is true.
    - Modified s3_setup to remove bucket name from prefix when s3ForcePathStyle is true.
    - Added test coverage for s3ForcePathStyle cases.
    
    * Fixed console logs to report paths actually used (which may be not at amazonaws.com).
    
    * Various tweaks to tests so that they pass in various configurations.
    
    * Modified test to account for path issues in evaluation on windows latest with node 20, 22.

--- test/s3_setup.test.js
+++ test/s3_setup.test.js
@@ -3,54 +3,207 @@
 const s3_setup = require('../lib/util/s3_setup.js');
 const test = require('tape');
 
-test('should propertly detect s3 bucket and prefix', (t) => {
+test('should properly detect virtual host s3 bucket', (t) => {
+  const opts = {
+    hosted_path: 'https://bucket-name.s3.us-west-2.amazonaws.com'
+  };
+  const result = s3_setup.detect(opts);
+  t.equal(result.prefix, '');
+  t.equal(result.bucket, 'bucket-name');
+  t.equal(result.region, 'us-west-2');
+  t.end();
+});
+
+test('should properly detect virtual host s3 bucket and prefix', (t) => {
+  const opts = {
+    hosted_path: 'https://bucket-name.s3.us-west-2.amazonaws.com/prefix'
+  };
+  const result = s3_setup.detect(opts);
+  t.equal(result.prefix, 'prefix');
+  t.equal(result.bucket, 'bucket-name');
+  t.equal(result.region, 'us-west-2');
+  t.end();
+});
+
+test('should properly detect virtual host s3 bucket with legacy dash format', (t) => {
+  const opts = {
+    hosted_path: 'https://bucket-name.s3-us-west-2.amazonaws.com'
+  };
+  const result = s3_setup.detect(opts);
+  t.equal(result.prefix, '');
+  t.equal(result.bucket, 'bucket-name');
+  t.equal(result.region, 'us-west-2');
+  t.end();
+});
+
+test('should properly detect virtual host s3 bucket with legacy dash format and prefix', (t) => {
+  const opts = {
+    hosted_path: 'https://bucket-name.s3-us-west-2.amazonaws.com/prefix'
+  };
+  const result = s3_setup.detect(opts);
+  t.equal(result.prefix, 'prefix');
+  t.equal(result.bucket, 'bucket-name');
+  t.equal(result.region, 'us-west-2');
+  t.end();
+});
+
+
+test('should properly detect s3 bucket with dashes', (t) => {
   const opts = {
     hosted_path: 'https://bucket-with-dashes.s3-us-west-1.amazonaws.com'
   };
-  const result = {};
-  s3_setup.detect(opts, result);
+  const result = s3_setup.detect(opts);
   t.equal(result.prefix, '');
   t.equal(result.bucket, 'bucket-with-dashes');
   t.equal(result.region, 'us-west-1');
   t.end();
 });
 
-test('should propertly detect s3 bucket and prefix with dots', (t) => {
+test('should properly detect s3 bucket with dots', (t) => {
   const opts = {
-    hosted_path: 'https://bucket.with.dots.s3.amazonaws.com/prefix'
+    hosted_path: 'https://bucket.with.dots.s3-us-west-1.amazonaws.com'
   };
-  const result = {};
-  s3_setup.detect(opts, result);
-  t.equal(result.prefix, 'prefix');
+  const result = s3_setup.detect(opts);
+  t.equal(result.prefix, '');
   t.equal(result.bucket, 'bucket.with.dots');
-  t.equal(result.region, 'us-east-1');
+  t.equal(result.region, 'us-west-1');
   t.end();
 });
 
-test('should propertly detect modern s3 bucket and prefix', (t) => {
+test('should properly default to us-east-1 when no region provided (legacy standard region)', (t) => {
   const opts = {
-    hosted_path: 'https://bucket-name.s3.us-east-1.amazonaws.com'
+    hosted_path: 'https://bucket-name.s3.amazonaws.com'
   };
-  const result = {};
-  s3_setup.detect(opts, result);
+  const result = s3_setup.detect(opts);
   t.equal(result.prefix, '');
   t.equal(result.bucket, 'bucket-name');
   t.equal(result.region, 'us-east-1');
   t.end();
 });
 
-test('should propertly detect compatible s3 bucket and prefix', (t) => {
+test('should properly detect compatible s3 bucket', (t) => {
   const opts = {
-    host: 'https://storage.com',
     hosted_path: 'https://storage.com',
+    host: 'https://storage.com',
     bucket: 'bucket-name',
     region: 'us-east-1'
   };
-  const result = {};
-  s3_setup.detect(opts, result);
+  const result = s3_setup.detect(opts);
   t.equal(result.prefix, '');
   t.equal(result.bucket, 'bucket-name');
   t.equal(result.region, 'us-east-1');
   t.equal(result.endpoint, 'https://storage.com');
+  t.end();
+});
+
+test('should properly detect compatible s3 bucket with path style url', (t) => {
+  const opts = {
+    hosted_path: 'https://storage.com/bucket-name/', // set by versioning from package.json definitions with trailing slash
+    host: 'https://storage.com',
+    bucket: 'bucket-name',
+    region: 'us-east-1',
+    s3ForcePathStyle: true
+  };
+  const result = s3_setup.detect(opts);
+  t.equal(result.prefix, ''); // bucket name should be removed from prefix
+  t.equal(result.bucket, 'bucket-name');
+  t.equal(result.region, 'us-east-1');
+  t.equal(result.endpoint, 'https://storage.com');
+  t.end();
+});
+
+test('should properly detect compatible s3 bucket with path style url', (t) => {
+  const opts = {
+    hosted_path: 'https://storage.com', // set by versioning from package.json definitions
+    host: 'https://storage.com',
+    bucket: 'bucket-name',
+    region: 'us-east-1',
+    s3ForcePathStyle: false
+  };
+  const result = s3_setup.detect(opts);
+  t.equal(result.prefix, ''); // bucket name should be removed from prefix
+  t.equal(result.bucket, 'bucket-name');
+  t.equal(result.region, 'us-east-1');
+  t.equal(result.endpoint, 'https://storage.com');
+  t.end();
+});
+
+test('should error trying to parse invalid s3 url (path style)', (t) => {
+  const opts = {
+    hosted_path: 'https://s3.us-west-2.amazonaws.com/mybucket/' // no bucket name
+  };
+  let result = {};
+  try {
+    // eslint-disable-next-line no-unused-vars
+    result = s3_setup.detect(opts);
+  } catch (e) {
+    const expectedMessage = 'Could not parse s3 bucket name from virtual host url.';
+    t.equal(e.message, expectedMessage);
+  }
+
+  t.equal(result.prefix, undefined);
+  t.equal(result.bucket, undefined);
+  t.equal(result.region, undefined);
+  t.end();
+});
+
+test('should error trying to parse invalid s3 url (not s3)', (t) => {
+  const opts = {
+    hosted_path: 'https://storage.com' // domain url without bucket/region defined
+  };
+  let result = {};
+  try {
+    // eslint-disable-next-line no-unused-vars
+    result = s3_setup.detect(opts);
+  } catch (e) {
+    const expectedMessage = 'Could not parse s3 bucket name from virtual host url.';
+    t.equal(e.message, expectedMessage);
+  }
+
+  t.equal(result.prefix, undefined);
+  t.equal(result.bucket, undefined);
+  t.equal(result.region, undefined);
+  t.end();
+});
+
+test('should error trying to parse invalid s3 when using miss-configured compatible host (no bucket name)', (t) => {
+  const opts = {
+    host: 'https://storage.com',
+    hosted_path: 'https://storage.com',
+    region: 'us-east-1'
+  };
+  let result = {};
+  try {
+    // eslint-disable-next-line no-unused-vars
+    result = s3_setup.detect(opts);
+  } catch (e) {
+    const expectedMessage = 'Could not parse s3 bucket name from virtual host url.';
+    t.equal(e.message, expectedMessage);
+  }
+
+  t.equal(result.prefix, undefined);
+  t.equal(result.bucket, undefined);
+  t.equal(result.region, undefined);
+  t.end();
+});
+
+test('should error trying to parse invalid s3 when using miss-configured compatible host (no region name)', (t) => {
+  const opts = {
+    host: 'https://storage.com',
+    hosted_path: 'https://storage.com',
+    bucket: 'bucket-name'
+  };
+  let result = {};
+  try {
+    // eslint-disable-next-line no-unused-vars
+    result = s3_setup.detect(opts);
+  } catch (e) {
+    const expectedMessage = 'Could not parse s3 bucket name from virtual host url.';
+    t.equal(e.message, expectedMessage);
+  }
+
+  t.equal(result.prefix, undefined);
+  t.equal(result.bucket, undefined);
+  t.equal(result.region, undefined);
   t.end();
 });

3e216b3b1ae46c7c2df43ef723b873561ea89185
rafaykh90 <rafay.khan@mapbox.com>
2025-11-26 13:49:46 +0200

    fix tests

--- test/private-binary.test.js
+++ test/private-binary.test.js
@@ -5,6 +5,8 @@ const path = require('path');
 const test = require('tape');
 const nock = require('nock');
 const install = require('../lib/install.js');
+const os = require('os');
+const rimraf = require('rimraf');
 
 // Dummy tar.gz data - contains a blank directory
 const targz = 'H4sICPr8u1oCA3gudGFyANPTZ6A5MDAwMDc1VQDTZhAaCGA0hGNobGRqZm5uZmxupGBgaGhiZsKgYMpAB1BaXJJYBHRKYk5pcioedeUZqak5+D2J5CkFhlEwCkbBKBjkAAAyG1ofAAYAAA==';
@@ -12,42 +14,58 @@ const targz = 'H4sICPr8u1oCA3gudGFyANPTZ6A5MDAwMDc1VQDTZhAaCGA0hGNobGRqZm5uZmxup
 // Determine the project root (where package.json with @mapbox/node-pre-gyp exists)
 const projectRoot = path.join(__dirname, '..');
 
+// Helper to clean mock S3 directory
+function cleanMockS3() {
+  const mockDir = path.join(os.tmpdir(), 'mock');
+  if (fs.existsSync(mockDir)) {
+    rimraf.sync(mockDir);
+  }
+}
+
 test('should fallback to authenticated download on 403 Forbidden', (t) => {
+  // Clean mock S3 to ensure deterministic behavior
+  cleanMockS3();
   process.env.node_pre_gyp_mock_s3 = 'true';
   process.env.AWS_ACCESS_KEY_ID = 'mock-key';
   process.env.AWS_SECRET_ACCESS_KEY = 'mock-secret';
 
   const origin = 'https://npg-mock-bucket.s3.us-east-1.amazonaws.com';
-  nock.cleanAll();
+  const run = require('./run.util.js');
+  const app = { name: 'app1', args: '' };
 
-  // Mock the public HTTPS request to return 403
-  const publicScope = nock(origin)
-    .get(/\/node-pre-gyp\/node-pre-gyp-test-app1\/v0.1.0\/Release\/node-v\d+-\S+.tar.gz/)
-    .reply(403, 'Forbidden');
-
-  const opts = {
-    opts: {
-      'build-from-source': false,
-      'update-binary': true
-    }
-  };
-
-  // cd into app directory from project root
-  const appDir = path.join(projectRoot, 'test', 'app1');
-  process.chdir(appDir);
-  opts.package_json = JSON.parse(fs.readFileSync('./package.json'));
-  opts.package_json.binary.host = origin;
-
-  install(opts, [], (err) => {
-    delete process.env.node_pre_gyp_mock_s3;
-    delete process.env.AWS_ACCESS_KEY_ID;
-    delete process.env.AWS_SECRET_ACCESS_KEY;
-
-    t.ok(publicScope.isDone(), 'Public HTTPS request was attempted');
-    t.notOk(err, 'Authenticated download succeeded');
+  // First build and publish to mock S3 to create the file
+  run('node-pre-gyp', 'configure build package publish', '', app, {}, (buildErr) => {
+    t.ifError(buildErr, 'Build and publish to mock S3 should succeed');
 
     nock.cleanAll();
-    t.end();
+    const publicScope = nock(origin)
+      .get(/\/node-pre-gyp\/node-pre-gyp-test-app1\/v0.1.0\/Release\/node-v\d+-\S+.tar.gz/)
+      .reply(403, 'Forbidden');
+
+    const appDir = path.join(projectRoot, 'test', 'app1');
+    process.chdir(appDir);
+    const installOpts = {
+      opts: {
+        'build-from-source': false,
+        'update-binary': true
+      },
+      package_json: JSON.parse(fs.readFileSync('./package.json'))
+    };
+    installOpts.package_json.binary.host = origin;
+
+    // Run install - should get 403 from HTTP, then succeed with authenticated S3
+    install(installOpts, [], (err) => {
+      delete process.env.node_pre_gyp_mock_s3;
+      delete process.env.AWS_ACCESS_KEY_ID;
+      delete process.env.AWS_SECRET_ACCESS_KEY;
+
+      t.ok(publicScope.isDone(), 'Public HTTPS request was attempted (got 403)');
+      t.ifError(err, 'Authenticated S3 download should succeed after 403');
+
+      nock.cleanAll();
+      cleanMockS3();
+      t.end();
+    });
   });
 });
 
